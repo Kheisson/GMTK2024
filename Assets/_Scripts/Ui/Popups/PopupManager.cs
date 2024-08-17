4f2798c4921eb672e8e76d11a668de136edee1b3
Kheisson <igor.kheison@gmail.com>
2024-08-17 18:33:37 +0300

    fix time issues with popup

--- Assets/_Scripts/Ui/Popups/PopupManager.cs
+++ Assets/_Scripts/Ui/Popups/PopupManager.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections.Generic;
 using _Scripts.Infra;
 using Cysharp.Threading.Tasks;
-using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace _Scripts.Ui.Popups
 {
     public class PopupManager
     {
+        public event Action OnPopupOpen;
+        public event Action OnPopupClose;
+        
         private readonly Stack<PopupView> _popups = new Stack<PopupView>();
         private readonly PopupCollection _popupCollection; 
+        
         private UiManager _uiManager;
         
         private UiManager UiManager => _uiManager ??= ServiceLocator.GetService<UiManager>();
@@ -21,6 +26,7 @@ namespace _Scripts.Ui.Popups
         public async UniTask ShowPopupAsync(EPopup id)
         {
             var popupPrefab = _popupCollection.GetPopup(id);
+            
             if (popupPrefab == null || IsPopupOpen(popupPrefab)) return;
 
             var popupInstance = Object.Instantiate(popupPrefab, UiManager.GetSettingsCanvas().gameObject.transform);
@@ -33,7 +39,7 @@ namespace _Scripts.Ui.Popups
             _popups.Push(popupInstance);
             popupInstance.gameObject.SetActive(true);
             await popupInstance.ShowAsync();
-            Time.timeScale = 0;
+            OnPopupOpen?.Invoke();
         }
 
         public async UniTask ClosePopupAsync()
@@ -49,6 +55,8 @@ namespace _Scripts.Ui.Popups
             {
                 _popups.Peek().gameObject.SetActive(true);
             }
+            
+            OnPopupClose?.Invoke();
         }
         
         private bool IsPopupOpen(PopupView popupPrefab)
