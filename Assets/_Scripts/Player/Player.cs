d1e83d6a7db301ee9964ff73db55785d42223c92
Kheisson <igor.kheison@gmail.com>
2024-08-17 13:38:01 +0300

    add outline and scaling logic

---
+++ Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,113 @@
+using Scalable;
+using UnityEngine;
+
+namespace Player
+{
+    public class Player : MonoBehaviour, IPlayer
+    {
+        [SerializeField] private float scaleStep = 1.0f;
+        public EScaleAbility scaleAbility;
+        private IScalable _scalableObject;
+
+        private void Update()
+        {
+            Move();
+            InteractWithObject();
+#if UNITY_EDITOR
+            SwitchScaleAbility();
+#endif
+        }
+
+        public void Move()
+        {
+            Vector3 moveDirection = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W)) moveDirection += Vector3.up;
+            if (Input.GetKey(KeyCode.S)) moveDirection += Vector3.down;
+            if (Input.GetKey(KeyCode.A)) moveDirection += Vector3.left;
+            if (Input.GetKey(KeyCode.D)) moveDirection += Vector3.right;
+
+            transform.position += moveDirection * Time.deltaTime;
+        }
+
+        public void InteractWithObject()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PerformScale(scaleAbility, EScaleCommand.ScaleUp);
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                PerformScale(scaleAbility, EScaleCommand.ScaleDown);
+            }
+        }
+
+        private void PerformScale(EScaleAbility ability, EScaleCommand scaleCommand)
+        {
+            if (_scalableObject == null) return;
+
+            var direction = ability switch
+            {
+                EScaleAbility.ScaleX => Vector3.right,
+                EScaleAbility.ScaleY => Vector3.up,
+                _ => Vector3.zero,
+            };
+
+            var scaleAmount = scaleCommand == EScaleCommand.ScaleUp ? scaleStep : -scaleStep;
+            _scalableObject.ScaleObject(direction, scaleAmount);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.name.Contains(nameof(Cube)))
+            {
+                Debug.Log("Collision with cube");
+                _scalableObject = other.gameObject.GetComponent<IScalable>();
+                
+                if (_scalableObject != null)
+                {
+                    Debug.Log("Cube is scalable");
+                    _scalableObject.OnScaleSuccess += HandleScaleSuccess;
+                    _scalableObject.OnScaleFailure += HandleScaleFailure;
+                }
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.name.Contains(nameof(Cube)))
+            {
+                Debug.Log("Exit collision with cube");
+                if (_scalableObject != null)
+                {
+                    Debug.Log("Cube is no longer scalable");
+                    _scalableObject.OnScaleSuccess -= HandleScaleSuccess;
+                    _scalableObject.OnScaleFailure -= HandleScaleFailure;
+                }
+                _scalableObject = null;
+            }
+        }
+
+        private void HandleScaleSuccess()
+        {
+            Debug.Log("Scale completed successfully.");
+        }
+
+        private void HandleScaleFailure()
+        {
+            Debug.Log("Scale operation unsuccessful.");
+        }
+        
+        #if UNITY_EDITOR
+        
+        private void SwitchScaleAbility()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                scaleAbility = scaleAbility == EScaleAbility.ScaleX ? EScaleAbility.ScaleY : EScaleAbility.ScaleX;
+            }
+        }
+        
+        #endif
+    }
+}
---
+++ Assets/_Scripts/Scalable/Cube.cs
@@ -0,0 +1,136 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Player;
+using UnityEngine;
+
+namespace Scalable
+{
+    [RequireComponent(typeof(Collider2D), typeof(Renderer))]
+    public class Cube : MonoBehaviour, IScalable
+    {
+        [SerializeField, ColorUsage(true, true)] private Color activeColor;
+        [SerializeField, ColorUsage(true, true)] private Color inactiveColor;
+        [SerializeField] private float minScale = 1.0f;
+        [SerializeField] private float overlapBoxThickness = 0.1f;
+        [SerializeField] private LayerMask collisionLayer; 
+        private readonly Collider2D[] _collisionResults = new Collider2D[10];
+        private Collider2D _collider2D;
+        private OutlineFx.OutlineFx _outlineFx;
+        public event Action OnScaleSuccess;
+        public event Action OnScaleFailure;
+
+        public bool CanScale { get; private set; } = true;
+        
+        private void Awake()
+        {
+            _collider2D = GetComponent<Collider2D>();
+            _outlineFx = GetComponent<OutlineFx.OutlineFx>();
+        }
+        
+        public void ScaleObject(Vector3 direction, float scaleAmount)
+        {
+            if (!CanScale)
+            {
+                return;
+            }
+            
+            var newScale = transform.localScale;
+            var scaleDelta = Vector3.zero;
+
+            if (direction == Vector3.right)
+            {
+                newScale.x = Mathf.Max(newScale.x + scaleAmount, minScale);
+                scaleDelta = new Vector3(scaleAmount / 2f, 0, 0);
+            }
+            else if (direction == Vector3.up)
+            {
+                newScale.y = Mathf.Max(newScale.y + scaleAmount, minScale);
+                scaleDelta = new Vector3(0, scaleAmount / 2f, 0);
+            }
+
+            if (ShouldScaleFail(direction, scaleAmount))
+            {
+                OnScaleFailure?.Invoke();
+                
+                return;
+            }
+            
+            var newPosition = transform.position + scaleDelta;
+
+            UniTask.Void(async () =>
+            {
+                var token = this.GetCancellationTokenOnDestroy();
+                CanScale = false;
+                
+                await UniTask.WhenAll(
+                    transform.DOScale(newScale, 0.5f).SetEase(Ease.InSine).WithCancellation(token),
+                    transform.DOMove(newPosition, 0.5f).SetEase(Ease.InSine).WithCancellation(token)
+                );
+
+                CanScale = true;
+                OnScaleSuccess?.Invoke();
+            });
+        }
+        
+        private bool ShouldScaleFail(Vector3 direction, float scaleAmount)
+        {
+            switch (scaleAmount)
+            {
+                case > 0 when WillCauseCollision(direction, scaleAmount):
+                case < 0 when (direction == Vector3.right && transform.localScale.x <= minScale) || 
+                              (direction == Vector3.up && transform.localScale.y <= minScale):
+                    return true;
+                default:
+                    return false; 
+            }
+        }
+
+        private bool WillCauseCollision(Vector3 direction, float scaleAmount)
+        {
+            var boxSize = Vector3.Scale(_collider2D.bounds.size, new Vector3(
+                direction == Vector3.right ? 1 : overlapBoxThickness,
+                direction == Vector3.up ? 1 : overlapBoxThickness,
+                1));
+
+            var boxCenter = transform.position + direction * scaleAmount / 2f;
+
+            var hitCount = Physics2D.OverlapBoxNonAlloc(boxCenter, boxSize, 0f, _collisionResults, collisionLayer);
+
+            for (var i = 0; i < hitCount; i++)
+            {
+                if (_collisionResults[i].gameObject != gameObject)
+                {
+                    return true;  
+                }
+            }
+
+            return false;  
+        }
+        
+        private void ActivateOutline(bool activate)
+        {
+            if (_outlineFx != null)
+            {
+                _outlineFx.enabled = activate;
+                _outlineFx.Color = activate ? activeColor : inactiveColor;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.TryGetComponent(out IPlayer player))
+            {
+                ActivateOutline(true);
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.TryGetComponent(out IPlayer player))
+            {
+                ActivateOutline(false);
+            }
+        }
+    }
+}
