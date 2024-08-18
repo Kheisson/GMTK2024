869d4f47e7d39bf8a03a19b73c6d0ba2666c5e5b
Nir Zaid <NirZaid6@gmail.com>
2024-08-18 12:27:35 +0300

    added logic for level transitioning

--- Assets/_Scripts/Levels/LevelEndHandler.cs
+++ Assets/_Scripts/Levels/LevelEndHandler.cs
@@ -1,39 +1,61 @@
 using System;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using Player;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Levels
 {
     public class LevelEndHandler : MonoBehaviour
     {
-        private Dictionary<EPlayerType, bool> playerStates = new Dictionary<EPlayerType, bool>()
+        [SerializeField] private float levelEndDelaySeconds = 0.5f;
+        private bool _hasLevelFinished;
+        private Dictionary<EPlayerType, bool> _playerStates;
+
+        private void Awake()
         {
-            [EPlayerType.X] = false,
-            [EPlayerType.Y] = false
-        };
-        
+            _playerStates = new Dictionary<EPlayerType, bool>()
+            {
+                [EPlayerType.X] = false,
+                [EPlayerType.Y] = false
+            };
+        }
+
         public void NotifyPlayerStateChanged(EPlayerType playerType, bool hasFinished)
         {
-            playerStates[playerType] = hasFinished;
+            _playerStates[playerType] = hasFinished;
             CheckForLevelTransition();
         }
 
         private void CheckForLevelTransition()
         {
+            if (_hasLevelFinished)
+            {
+                return;
+            }
+            
             foreach (EPlayerType type in Enum.GetValues(typeof(EPlayerType)))
             {
-                if (!playerStates.TryGetValue(type, out bool hasFinished) || !hasFinished)
+                if (!_playerStates.TryGetValue(type, out bool hasFinished) || !hasFinished)
                 {
                     return;
                 }
             }
 
+            _hasLevelFinished = true;
             StartLevelTransition();
         }
 
         private void StartLevelTransition()
         {
+            UniTask.WaitForSeconds(levelEndDelaySeconds).ContinueWith(LoadNextLevel);
+        }
+
+        private void LoadNextLevel()
+        {
+            var currentActiveSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentActiveSceneIndex + 1);
         }
     }
 }
